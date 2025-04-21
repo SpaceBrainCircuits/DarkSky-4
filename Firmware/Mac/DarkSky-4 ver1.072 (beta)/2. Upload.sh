47a43ca91d9468e5170034c2b65e16d416d490e5
Austin Luberda <austinluberda@Austin-mac.attlocal.net>
2025-04-20 22:41:13 -0500

    revised upload.sh file

--- Firmware/Mac/DarkSky-4 ver1.072 (beta)/2. Upload.sh
+++ Firmware/Mac/DarkSky-4 ver1.072 (beta)/2. Upload.sh
@@ -4,4 +4,4 @@ cd "$(dirname "$0")"
 
 chmod +x avrdude
 
-./avrdude -C./avrdude.conf -v -D -patmega32u4 -cavr109 -P/dev/tty.usbmodem14301 -b57600 -Uflash:w:./FourVoice1.063.hex:i
+./avrdude -C./avrdude.conf -v -D -patmega32u4 -cavr109 -P/dev/tty.usbmodem14301 -b57600 -Uflash:w:./FourVoice1.072.hex:i
